cc52ecf78eac65cdb51c6ffa8da2737500a80c93
Thais Franca <tfrancag@gmail.com>
2022-03-22 14:11:51 -0300

    feat. Added Lato font to indexHTML. Added form to sing in Page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,50 @@
 import React, { useState } from "react";
-import { AuthorizationScreen, TitleScreen, Title, SubTitle } from "./style";
+import { Link } from "react-router-dom";
+import {
+  AuthorizationScreen,
+  TitleScreen,
+  Title,
+  SubTitle,
+  Form,
+  Input,
+  Button,
+  StyledLink,
+} from "./style";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(email);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submit was called");
+  }
   return (
     <AuthorizationScreen>
       <TitleScreen>
         <Title>linkr</Title>
         <SubTitle>save, share and discover the best links on the web</SubTitle>
       </TitleScreen>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          placeholder="e-mail"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          value={email}
+          required
+        />
+        <Input
+          placeholder="senha"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+          value={password}
+          required
+        />
+        <Button type="Log In">Log In</Button>
+        <StyledLink to="/sign-up">First time? Create an account!</StyledLink>
+      </Form>
     </AuthorizationScreen>
   );
 }
--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { Link } from "react-router-dom";
 const AuthorizationScreen = styled.main`
   width: 100vw;
   height: 100vh;
@@ -30,4 +30,64 @@ const SubTitle = styled.span`
   font-family: "Oswald";
 `;
 
-export { AuthorizationScreen, TitleScreen, Title, SubTitle };
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 51px 0 55px;
+`;
+
+const Input = styled.input`
+  height: 70px;
+  margin-bottom: 15px;
+  padding-left: 17px;
+
+  border-radius: 6px;
+  border: none;
+  background-color: #ffffff;
+
+  font-size: 27px;
+  font-weight: 700;
+  line-height: 40px;
+
+  color: #9f9f9f;
+  font-family: "Oswald";
+`;
+
+const Button = styled.button`
+  height: 70px;
+  width: 100%;
+  margin-bottom: 15px;
+  justify-content: center;
+
+  border-radius: 6px;
+  border: none;
+  background-color: #1877f2;
+
+  font-size: 27px;
+  font-weight: 700;
+  line-height: 40px;
+
+  color: #ffffff;
+  font-family: "Oswald";
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 30px;
+
+  font-family: "Lato";
+  color: #ffffff;
+`;
+export {
+  AuthorizationScreen,
+  TitleScreen,
+  Title,
+  SubTitle,
+  Form,
+  Input,
+  Button,
+  StyledLink,
+};
