d48c74aa8b5ad3cae4d4eceeaf776c60337e9328
Thais Franca <tfrancag@gmail.com>
2022-03-26 10:05:33 -0300

    feat. Added cometic solution to show like and dislike by user dynamically withou havind to reload info from back-end at each click

--- src/pages/Timeline/PostsItems/LikesPost.js
+++ src/pages/Timeline/PostsItems/LikesPost.js
@@ -1,11 +1,12 @@
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { useEffect, useState } from "react";
-import { Icon } from "./LikesStyle";
+import { Icon, LikesInfo } from "./LikesStyle";
 import useAuth from "../../../hooks/userAuth";
 import api from "../../../services/api";
-//tem que conferir se p usuário está logao. Se nao estiver, não pode curtir
+
 export default function LikesDisplay({ postId, likesNumber, likedByUser }) {
-  const [liked, setLiked] = useState(false);
+  const [liked, setLiked] = useState(likedByUser);
+  const [numberLikes, setNumberLikes] = useState(parseInt(likesNumber));
   const { auth } = useAuth();
   console.log(auth);
   console.log(postId);
@@ -15,6 +16,7 @@ export default function LikesDisplay({ postId, likesNumber, likedByUser }) {
   }, [liked]);
 
   async function updateLike() {
+    console.log("fui chamado");
     try {
       const { data } = await api.getLikes(auth);
       console.log(data);
@@ -29,16 +31,18 @@ export default function LikesDisplay({ postId, likesNumber, likedByUser }) {
     try {
       await api.toggleLike(auth, postId, liked);
       liked ? setLiked(false) : setLiked(true);
+      liked ? setNumberLikes(numberLikes - 1) : setNumberLikes(numberLikes + 1);
     } catch (error) {
       console.error(error);
     }
   }
+
   return (
     <>
-      <Icon liked={likedByUser} onClick={() => handleLike()}>
-        {likedByUser ? <FaHeart /> : <FaRegHeart />}
+      <Icon liked={liked} onClick={() => handleLike()}>
+        {liked ? <FaHeart /> : <FaRegHeart />}
       </Icon>
-      {likesNumber}
+      <LikesInfo>{numberLikes} Likes</LikesInfo>
     </>
   );
 }
--- src/pages/Timeline/PostsItems/LikesStyle.js
+++ src/pages/Timeline/PostsItems/LikesStyle.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 const Icon = styled.div`
   font-size: 20px;
   color: ${(props) => (props.liked ? "#AC0000" : "#FFFFFF")};
+  cursor: pointer;
 `;
 
-export { Icon };
+const LikesInfo = styled.div`
+  font-size: 11px;
+  line-height: 13.5px;
+`;
+export { Icon, LikesInfo };
